56bcd8e99570479ffe7a0766dada8fc5535b93c8
BejenaruIoanMatei <bejematei@gmail.com>
2024-04-22 14:37:04 +0300

    1: Implemented the logic of the game

--- Lab_7/src/main/java/org/example/Game.java
+++ Lab_7/src/main/java/org/example/Game.java
@@ -20,15 +20,39 @@ public class Game
         player.setGame(this);
     }
 
-    public void play()
-    {
-        for (Player player : players)
-        {
-            new Thread(player).start();
+    public void play() {
+        List<Thread> playerThreads = new ArrayList<>();
+        for (Player player : players) {
+            Thread playerThread = new Thread(player);
+            playerThreads.add(playerThread);
+            playerThread.start();
         }
+        for (Thread playerThread : playerThreads) {
+            try {
+                playerThread.join(); // Wait for all player threads to finish
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Player winner = determineWinner();
+        System.out.println("-------Anuntam castigatorul--------");
+        System.out.println("Winner is: " + winner.getName() + " with " + winner.getPoints() + " points.");
     }
+
+
 
     public Bag getBag() {
         return bag;
     }
+
+    public Player determineWinner() {
+        Player winner = players.get(0);
+        for (Player player : players) {
+            if (player.getPoints() > winner.getPoints()) {
+                winner = player;
+            }
+        }
+        return winner;
+    }
+
 }
--- Lab_7/src/main/java/org/example/Player.java
+++ Lab_7/src/main/java/org/example/Player.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-class Player implements Runnable
-{
+public class Player implements Runnable {
     private String name;
     private Game game;
+    private List<List<Tile>> sequences = new ArrayList<>();
+    private int points = 0;
     private boolean playerRunning = true;
 
     public Player(String name) {
@@ -19,18 +20,50 @@ class Player implements Runnable
         this.game = game;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
-    public void run()
-    {
-        while (playerRunning)
-        {
+    public void run() {
+        while (playerRunning) {
             List<Tile> tiles = game.getBag().extractTiles(2);
-            if (tiles.isEmpty())
-            {
+            if (tiles.isEmpty()) {
                 playerRunning = false;
                 break;
             }
             System.out.println(name + " a extras " + tiles);
+            updateSequences(tiles);
         }
+        System.out.println(name + " a terminat. Secvente: " + sequences);
+    }
+
+    private void updateSequences(List<Tile> tiles) {
+        for (Tile tile : tiles) {
+            for (List<Tile> sequence : sequences) {
+                if (isValidSequence(sequence, tile)) {
+                    sequence.add(tile);
+                    points = Math.max(points, sequence.size());
+                    return;
+                }
+            }
+            List<Tile> newSequence = new ArrayList<>();
+            newSequence.add(tile);
+            sequences.add(newSequence);
+            points = Math.max(points, 1);
+        }
+    }
+
+    private boolean isValidSequence(List<Tile> sequence, Tile newTile) {
+        Tile lastTile = sequence.get(sequence.size() - 1);
+        return lastTile.getNumber2() == newTile.getNumber1();
+    }
+
+    public int getPoints() {
+        return points;
     }
 }
